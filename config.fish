a9b93c2512a0010a9eca16a591043dabd8e945c6
Andrew Curtis-Black <andrew.curtis-black@pg.canterbury.ac.nz>
2018-05-02 19:03:01 +1200

    Removed (hopefully unneeded) OMF configs

--- config.fish
+++ config.fish
@@ -1,11 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
 # Bobthefish
 set -g theme_display_vagrant yes
 set -g theme_display_hostname ssh
